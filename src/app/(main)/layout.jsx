0c496547de70efa5e7581032679b45e9e63c88e8
Aseel Rahman <132919956+aseelrahman@users.noreply.github.com>
2026-09-12 19:36:03 -0400

    refactor: move home page and chrome into (main) route group

---
+++ src/app/(main)/layout.jsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/Navbar";
+
+const MainLayout = ({ children }) => {
+  return (
+    <>
+      <Navbar />
+      <main className="max-w-7xl mx-auto flex-1">{children}</main>
+    </>
+  );
+};
+
+export default MainLayout;
