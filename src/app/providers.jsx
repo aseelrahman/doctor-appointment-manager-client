edf987a268006554c416fd9b9e6439b11b031f85
Aseel Rahman <132919956+aseelrahman@users.noreply.github.com>
2026-09-20 22:38:53 -0400

    feat: add toast feature

--- src/app/providers.jsx
+++ src/app/providers.jsx
@@ -1,15 +1,12 @@
 "use client";
 
+import { Toast } from "@heroui/react";
 import { ThemeProvider } from "next-themes";
 
 export function Providers({ children }) {
   return (
-    <ThemeProvider
-      attribute="class"
-      enableSystem={false}
-      defaultTheme="dark"
-      
-    >
+    <ThemeProvider attribute="class" enableSystem={false} defaultTheme="dark">
+      <Toast.Provider />
       {children}
     </ThemeProvider>
   );
