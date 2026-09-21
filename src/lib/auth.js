81d081e6c47ec74830325b6a9e7d77a68ff508f6
Aseel Rahman <132919956+aseelrahman@users.noreply.github.com>
2026-09-20 22:38:26 -0400

    chore: import missing mongodbAdapter and MongoClient

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,4 +1,6 @@
+import { mongodbAdapter } from "@better-auth/mongo-adapter";
 import { betterAuth } from "better-auth";
+import { MongoClient } from "mongodb";
 
 const client = new MongoClient(process.env.MONGODB_URI);
 const db = client.db("doctimedb");
